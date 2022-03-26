ad2a7372f47675540fbb64da006687a04c91a817
ilyaUsoltsev <ilia.a.usoltsev@gmail.com>
2022-03-26 22:12:46 +0300

    page container styles fix

--- client/src/styles/page/page.styles.ts
+++ client/src/styles/page/page.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledPageContainer = styled.div`
   margin: 0 auto;
   background-color: rgb(248, 246, 246);
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
 `;
 
